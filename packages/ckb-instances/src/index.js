59bdd2f24bded5e2a752dfd2c5c9338b9b97b295
Phil <qft.gtr@gmail.com>
2020-07-25 21:55:37 +0800

    Update index.js

--- packages/ckb-instances/src/index.js
+++ packages/ckb-instances/src/index.js
@@ -2,5 +2,4 @@ export { default } from './instanceChannel'
 
 export { default as InstanceList } from './InstanceListWithTerminal'
 export { default as NodeVersionInstaller } from './NodeInstaller/NodeVersionInstaller'
-export { default as NodeVersionSelector } from './NodeInstaller/NodeVersionSelector'
 export { default as CkbIndexerInstaller } from './NodeInstaller/CkbIndexerInstaller'
