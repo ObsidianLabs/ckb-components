a041475d546317f7084e68e85b24e3ea78c6e825
Phil <qft.gtr@gmail.com>
2020-09-17 13:18:53 +0800

    remove GlobalModals and autoUpdate

--- packages/ckb-welcome/src/index.js
+++ packages/ckb-welcome/src/index.js
@@ -1,7 +1,2 @@
 export { default } from './Welcome'
 export { default as checkDependencies } from './Welcome/checkDependencies'
-
-export { default as GlobalModals } from './GlobalModals'
-export { default as globalModalManager } from './GlobalModals/globalModalManager'
-
-export { default as autoUpdater } from './autoUpdater'
