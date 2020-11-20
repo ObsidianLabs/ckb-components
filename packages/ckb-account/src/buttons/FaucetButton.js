750d6b5d223cec11fa8943c9210a5cfec508e862
Phil <qft.gtr@gmail.com>
2020-11-20 19:00:37 +0800

    Update FaucetButton.js

--- packages/ckb-account/src/buttons/FaucetButton.js
+++ packages/ckb-account/src/buttons/FaucetButton.js
@@ -8,37 +8,36 @@ import notification from '@obsidians/notification'
 
 export default class FaucetButton extends PureComponent {
   claim = async () => {
-    let faucetUrl
-    if (this.props.network === 'ckb-aggron') {
-      faucetUrl = 'https://faucet.nervos.org/claim_events'
-    } else {
+    const address = this.props.address
+    this.notification = notification.info('Claiming CKB...', `Trying to claim CKB tokens for <b>${address}</b>`, 0)
+    let res, result
+    try {
+      res = await fetch('https://ckb.obsidians.io/api/v1/faucet', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ address })
+      })
+      result = await res.json()
+    } catch (e) {
+      console.warn(e)
+    }
+    this.notification.dismiss()
+    if (!result) {
+      notification.error('Failed', 'Unknown error')
       return
     }
-
-    window.open('https://faucet.nervos.org', '_blank', 'noopener,noreferrer')
-  
-    // TODO
-    // this.notification = notification.info('Claiming CKB...', `Trying to claim CKB tokens for <b>${this.props.address}</b>`, 0)
-    // let result
-    // try {
-    //   // TODO: GET request to retrieve csrf
-    //   const res = await fetch(faucetUrl,)
-    //   result = await res.json()
-
-    //   // TODO: POST request with csrf token
-    // } catch (e) {
-    //   console.log(e)
-    // }
-    // this.notification.dismiss()
-    // if (!result) {
-    //   notification.error('Failed', 'Unknown error')
-    //   return
-    // }
-    // if (result.code) {
-    //   notification.error('Failed', result.message)
-    // } else {
-    //   notification.success('CFX Claimed', `Claimed 100 CFX for <b>${this.props.address}</b>`)
-    // }
+    if (res.status >= 400) {
+      if (result.address_hash) {
+        notification.error('Failed', result.address_hash)
+      } else {
+        notification.error('Failed', 'Unknown error')
+      }
+    } else {
+      notification.success('CKB Claimed', `Claimed 5000 CFX for <b>${address}</b>`)
+    }
   }
 
   render () {
